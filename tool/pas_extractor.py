1d390fcdd771a119aadfcac28417efe12a9aaba1
Yu Sawai <yusawai.null@gmail.com>
2012-08-19 17:07:16 +0900

    modified pas_extractor

--- tool/pas_extractor.py
+++ tool/pas_extractor.py
@@ -129,22 +129,24 @@ def extract(input_prefix, output_prefix):
 def cicp_extract(input_prefix, output_prefix):
     native_list = open('./Native.txt', 'r').readlines()
     native_list = [fn.strip('\n')[2:] for fn in native_list]
+    print native_list
     foreign_list = open('./Foreign.txt', 'r').readlines()
     foreign_list = [fn.strip('\n')[2:] for fn in foreign_list]
-
+    print foreign_list
+    
     for i, f in enumerate(native_list):
         logging.debug(('Native:  Processing file no.\t %d (%d remaining...)'%(i+1,(num_f-i-1))))
         pax = PasExtractor(os.path.join(input_prefix, f + ".txt.parsed"))
         tmpc = collections.Counter(pax.extract())
         pastriples_counter = pastriples_counter + tmpc
-        output2file(input_prefix, output_prefix, pastriples_counter)
+        output2file(input_prefix, output_prefix+"Native", pastriples_counter)
 
     for i, f in enumerate(foreign_list):
         logging.debug(('Foreign: Processing file no.\t %d (%d remaining...)'%(i+1,(num_f-i-1))))
         pax = PasExtractor(os.path.join(input_prefix, f + ".txt.parsed"))
         tmpc = collections.Counter(pax.extract())
         pastriples_counter = pastriples_counter + tmpc
-        output2file(input_prefix, output_prefix, pastriples_counter)
+        output2file(input_prefix, output_prefix+"NonNative", pastriples_counter)
 
 
 
