779f9b3089b4eeb00d2982ac666f1500e29685d9
Yu Sawai <yusawai.null@gmail.com>
2012-09-11 23:36:58 +0900

    added samples of irstlm

--- src/test_detector.py
+++ src/test_detector.py
@@ -19,9 +19,18 @@ class TestLMDetector:
     def setUp(self):
         self.corpuspath = "../sandbox/fce_corpus/fce_processed.pickle"
         self.detector = LM_Detector(self.corpuspath)
+        self.testlm_path = "../sandbox/irstlm_sample/testlm.gz"
 
     @attr("makecase")
     def test_makecase(self):
         self.detector.make_cases()
+        print pformat(self.detector.testcases)
+        raise Exception
+
+    @attr("LM")
+    def test_LM(self):
+        self.detector.make_cases()
+        self.detector.read_LM_and_PASLM(path_IRSTLM=self.testlm_path)
+        self.detector_LM_count()
         print pformat(self.detector.testcases)
         raise Exception
