58f12579bf8dc02d735525027ae3660448f60306
Yu Sawai <yusawai.null@gmail.com>
2012-10-20 18:40:53 +0900

    now classifier of bolt toolkit is implemented

--- src/classifier.py
+++ src/classifier.py
@@ -1,109 +1,175 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 # coding: utf-8
 '''
-Nyanco/nyanco.py
-Created on 11 Jan 2012
-
-
-@author: Yu Sawai
-@version: 0.001
+nyanco/src/classifer.py
 '''
-try:
-    import subprocess
-    import os
-    import cPickle as pickle
-    # nyanco modules
-    import corpusreader
-    import preprocessor
-    import lm_counter
-    import postprocessor
-    import nltk
-except ImportError:
-    print("Failed to import pickle module from any place")
+__author__ = "Yu Sawai"
+__copyright__ = "Copyright 2012, Yu Sawai"
+__version__ = "0.1"
+__status__ = "Prototyping"
+
+import os
+import sys
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+import cPickle as pickle
+import collections
+from collections import defaultdict
+from pprint import pformat
+from time import time
+import glob
+# Currently, assuming bolt online classifier toolkit as sgd/pegasos classifier
+# and scikit-learn as utilities and for svm models
+try: 
+    import bolt
+    from sklearn.feature_extraction import DictVectorizer
+    from sklearn import preprocessing
+    from svmlight_loader import *
+    import numpy as np
+except:
+    raise ImportError
+from feature_extractor import SimpleFeatureExtractor
 
 
-class FeatureExtractor(object):
-    '''
-    This gives Feature Dictionary for MaxEntClassifier and SVMs
-    '''
+class CaseMaker(object):
+    def __init__(self, verbcorpus_dir="", verbset_path="", model_dir="", npy_dir=""):
+        if not verbcorpus_dir and verbset_path and model_dir and npy_dir:
+            raise TypeError
+        else:
+            print "CaseMaker: successfully imported bolt and sklearn"
+        self.corpusdir = verbcorpus_dir
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        if not os.path.exists(npy_dir):
+            os.makedirs(npy_dir)
+        self.model_dir = model_dir
+        self.npy_dir = npy_dir
+        verbset_load = pickle.load(open(verbset_path,"rb"))
+        self.verbs = verbset_load["verbs"]
+        self.verbsets = verbset_load["verbset"]
+        vcorpus_filenames = glob.glob(os.path.join(self.corpusdir, "*.pkl2"))
+        v_names = [os.path.basename(path).split(".")[0] for path in vcorpus_filenames]
+        self.vcorpus_filedic = {vn : fn for (vn, fn) in zip(v_names, vcorpus_filenames)}
+
+
+    def make_fvectors(self):
+        """
+        Create feature vectors for given datasets, for classifiers as SVM^light format
+        using feature_extraction's classes
+        """
+        for setname, vset in self.verbsets.iteritems(): # setname is str, vset is list
+            print "CaseMaker make_fvectors: working on set '%s'"%setname
+            vectorizer = DictVectorizer(sparse=True)
+            # label_encoder = preprocessing.LabelEncoder()
+            _classname2id = {vt[0]: id for id, vt in enumerate(vset)}
+            _corpusdict = {}
+            _casedict = defaultdict(list)
+            _casedict["label2id"] = _classname2id
+            for v in [t[0] for t in vset]:
+                with open(self.vcorpus_filedic[v], "rb") as vcf:
+                    _corpusdict[v] = pickle.load(vcf)
+            for v, v_corpus in _corpusdict.iteritems():
+                _flist = []
+                _labellist_int = []
+                _labellist_str = []
+                _labelid = _classname2id[v]
+                for sid, s in enumerate(v_corpus):
+                    fe = SimpleFeatureExtractor(s, verb=v)
+                    fe.ngrams(n=7)
+                    # some other features!
+                    # then finally...
+                    _flist.append(fe.features)
+                    _labellist_int.append(_labelid)
+                    _labellist_str.append(v)
+                _casedict["X_str"] += _flist
+                _casedict["Y_str"] += _labellist_str
+                _casedict["Y"] += _labellist_int
+            fvectors_str = _casedict["X_str"]
+            try:
+                X = vectorizer.fit_transform(fvectors_str).toarray()
+                Y = np.array(_casedict["Y"])
+                dim_X = X.shape[1]
+            except UnboundLocalError, e:
+                print "CaseMaker make_fvectors: seems feature vector for the set %s is empty..."%setname
+                print pformat(e)
+                print fvectors_str
+                X = array([])
+                Y = array([])
+                dim_X = 0
+            dir_n = os.path.join(self.npy_dir, setname)
+            if not os.path.exists(dir_n):
+                os.makedirs(dir_n)
+            fn = os.path.join(dir_n, "dataset.svmlight")
+            fn_cdic = os.path.join(dir_n, "casedict.pkl2")
+            with open(fn, "wb") as f:
+                # np.savez(f, instances=X, labels=Y, dim=dim_X)
+                dump_svmlight_file(X, Y, f)
+            with open(fn_cdic, "wb") as pf:
+                cdic = {"setname":setname}
+                cdic["X_str"] = _casedict["X_str"]; cdic["Y_str"] = _casedict["Y_str"]
+                cdic["label2id"] = _casedict["label2id"]
+                pickle.dump(cdic, pf, -1)
+        print "CaseMaker make_fvectors: successfully done."
+
+#----------------------------------------------------------------------------------------------------
+class Classifier(object):
     def __init__(self):
-        self.featuredict = {}
         pass
 
+    def read(self, x, y):
+        raise NotImplementedError
 
-    def context_surface(self):
-        pass
+    def train(self):
+        raise NotImplementedError
 
-    
-    def context_pos(self):
-        pass
-
-
-    def head_noun(self):
-        pass
+    def predict(self):
+        raise NotImplementedError
 
 
-    def subject(self):
-        pass
-
-
-    def verb_info(self):
-        pass
-
-
-class TestFeatureExtractor(object):
+class BoltClassifier(Classifier):
     def __init__(self):
-        testsentence = []
+        self.models = ["sgd", "pegasos", "ap"]
         pass
 
-    def test_context_surface(self):
-        pass
+    def read_traincases(self, dataset_path=""):
+        try:
+            self.training_dataset = bolt.io.MemoryDataset.load(dataset_path)
+        except Exception, e:
+            print pformat(e)
+
+    def train(self, model="sgd", params={"reg":0.0001, "epochs": 20}):
+        if "reg" in params:
+            reg = params["reg"]
+        if "epochs" in params:
+            epochs = params["epochs"]
+        self.glm = bolt.GeneralizedLinearModel(m=self.training_dataset.dim, 
+                                               k=len(self.training_dataset.classes))
+        if model == "sgd":
+            trainer = bolt.SGD(bolt.Hinge(), reg=reg, epochs=epochs)
+        elif model == "pegasos":
+            trainer = bolt.PEGASOS(reg=reg, epochs=epochs)
+        elif model == "ap":
+            trainer = bolt.AveragedPerceptron(epochs=epochs)
+        else:
+            raise NotImplementedError
+        if model == "ap":
+            ap.train(self.glm, self.training_dataset, verbose=1, shuffle=True)
+        else:
+            ova = bolt.OVA(trainer)
+            ova.train(self.glm, self.training_dataset, verbose=1, shuffle=True)
 
 
-    def test_context_pos(self):
-        pass
-
-
-    def test_head_noun(self):
-        pass
-
-    
-    def test_subject(self):
-        pass
-
-
-    def test_verb_info(self):
-        pass
-
-
-
+    def predict(self, testset_path="", testset_array=[]):
+        if testset_array:
+            raise NotImplementedError
+        elif testset_path:
+            testset = bolt.io.MemoryDataset.load(testset_path)
+        pred = [p for p in self.glm.predict(testset.iterinstances())]
+        return pred
 
 
 def main():
-    corpusrdr = corpusreader.CLCReader()
-    corpusrdr.read()
-    corpusrdr
-    # queries_i_2_2, queries_c_2_2 = preprocessor.preprocess4ssgnc(corpusrdr.all_sents)
-#    print queries_i_2_2
-#    print queries_c_2_2
-    # print len(queries_c_2_2.keys())
-    # result_of_i_2_2 = lm_counter.ssgnc_search(queries_i_2_2)
-    # result_of_c_2_2 = lm_counter.ssgnc_search(queries_c_2_2)
-#    print zip(result_of_i_2_2,result_of_c_2_2)
-#    resultbase = [q for q in result_of_i_2_2 if not (q[0] == "0") ]
-    # num_all = len(result_of_i_2_2)
-    # num_nonzero = len([q for q in result_of_i_2_2 if not (q[1] == "0") ])
-    # goodlist = [t for t in zip(result_of_i_2_2,result_of_c_2_2)
-            # if (int(t[1][1]) > int(t[0][1])) and (t[0][1] != "0") and (t[1][1] != "0") ]
-    # num_correct = len(goodlist)
-    # print "\n"*3
-    # print "Processed RV tagged correction pairs: ", num_all
-    # print "Num. of non-zero result: ", num_nonzero
-    # print "Num. of gold prevails original: ", num_correct
-    # print "Acc:: n(Freq_i < Freq_c)/n(RV_tagged): ", 1.0*num_correct/num_all
-    # print "Non-Zero-Correction-rate:: n(correct)/n(nonzero): ", 1.0*num_correct/num_nonzero
-    # return True
-
+    pass
 
 if __name__=="__main__":
     import time
