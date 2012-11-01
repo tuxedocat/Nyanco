baf7fec3b1709dcd6d9198b28020f21c987076b9
Yu Sawai <yusawai.null@gmail.com>
2012-11-02 00:15:04 +0900

    updated

--- src/tool/lang8_preprocessor.py
+++ src/tool/lang8_preprocessor.py
@@ -14,47 +14,70 @@ import os
 from pprint import pformat
 from collections import defaultdict
 import cPickle as pickle
-import shelve
-import sqlite3
-import sqlalchemy
-from elixir import *
-
-metadata.bind = "sqlite:///eng.sqlite"
-metadata.echo = True
-
-class Sentence(Entity):
-    """
-    A simple representation for sentences in the lang-8 corpus
-    """
-    doc_id = Field(Integer)
-    sent_id = Field(Integer)
-    original_text = Field(UnicodeText)
-    annotated_raw = Field(UnicodeText)    
-    annotated_verb = Field(UnicodeText)
-    comments = Field(UnicodeText)
-    flags = Field(Unicode(30))
-    proficiency_level = Field(Unicode(10))
-    
-    def __repr__(self):
-        return '<Sentence::\ndoc_id "%d" \tsent_id "%d" \norg_text "%s" \nannotaion "%s"\nflags "%s">'%(self.doc_id, self.sent_id, self.original_text, self.annotated_raw, self.flags)
 
 
 
-"""
-session.close()
-for docid, tuple in enumerate(zip(docidlist, orgsentlist, annotatedlist)):
-    for sentid, sentpair in enumerate(zip(tuple[1], tuple[2])):
-        try:
+def savetoshelve(docids, orgs, annots, shelvedb):
+    for idx, docid in enumerate(docids):
+        did = str(docid)
+        for sentid, sentpair in enumerate(zip(orgs[idx], annots[idx])):
             if sentpair[1]:
-                Sentence(doc_id=tuple[0], 
-                         sent_id=sentid, 
-                         original_text=unicode(sentpair[0]), 
-                         annotated_raw=unicode("".join(sentpair[1])), 
-                         annotated_verb=u"", 
-                         comments=u"", 
-                         flags=u"", 
-                         proficiency_level=u"")
-        except:
-            pass
-session.commit()
-"""
+                sid = str(sentid)
+                try:
+                    shelvedb[did+"_"+sid] = {"original":unicode(sentpair[0]),
+                                             "correct_raw":unicode("".join(sentpair[1])),
+                                             "correct":"",
+                                             "comments":"",
+                                             "flags":[],
+                                             "proficiency_level":"",
+                                             "L1":""}
+
+                    print did+"_"+sid
+                except Exception, e:
+                    print e
+
+                    
+# import shelve
+# import sqlite3
+# import sqlalchemy
+# from elixir import *
+
+# metadata.bind = "sqlite:///eng.sqlite"
+# metadata.echo = True
+
+# class Sentence(Entity):
+#     """
+#     A simple representation for sentences in the lang-8 corpus
+#     """
+#     doc_id = Field(Integer)
+#     sent_id = Field(Integer)
+#     original_text = Field(UnicodeText)
+#     annotated_raw = Field(UnicodeText)    
+#     annotated_verb = Field(UnicodeText)
+#     comments = Field(UnicodeText)
+#     flags = Field(Unicode(30))
+#     proficiency_level = Field(Unicode(10))
+    
+#     def __repr__(self):
+#         return '<Sentence::\ndoc_id "%d" \tsent_id "%d" \norg_text "%s" \nannotaion "%s"\nflags "%s">'%(self.doc_id, self.sent_id, self.original_text, self.annotated_raw, self.flags)
+
+
+
+# """
+# session.close()
+# for docid, tuple in enumerate(zip(docidlist, orgsentlist, annotatedlist)):
+#     for sentid, sentpair in enumerate(zip(tuple[1], tuple[2])):
+#         try:
+#             if sentpair[1]:
+#                 Sentence(doc_id=tuple[0], 
+#                          sent_id=sentid, 
+#                          original_text=unicode(sentpair[0]), 
+#                          annotated_raw=unicode("".join(sentpair[1])), 
+#                          annotated_verb=u"", 
+#                          comments=u"", 
+#                          flags=u"", 
+#                          proficiency_level=u"")
+#         except:
+#             pass
+# session.commit()
+# """
